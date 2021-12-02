20942e48a05f62dab09bbd545380a667c217ce4d
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2021-12-02 22:29:25 +0100

    Some improvements

--- src/main/java/fr/lernejo/todo/Todo.java
+++ src/main/java/fr/lernejo/todo/Todo.java
@@ -1,6 +1,19 @@
 package fr.lernejo.todo;
 
 public class Todo extends TodoIdentity {
+	private final String message;
+	private final String author;
 
-	public Todo(String message, String author) {}
+	public Todo(String message, String author) {
+		this.message = message;
+		this.author = author;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
 }
