3342832872f3a119dfb67baa9f1b9f64ac765374
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2021-12-08 17:33:58 +0100

    Records are better

--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -9,23 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 
 @RestController
-public class TodoListController {
-	private final TodoRepository todos;
-
-	public TodoListController(TodoRepository repo) {
-		todos = repo;
-	}
+public record TodoListController(TodoRepository repository) {
 
 	@PostMapping("/api/todo")
 	public void addTodo(@RequestBody @NotNull Todo todo) {
 		TodoEntity entity = new TodoEntity();
 		entity.author = todo.author();
 		entity.message = todo.message();
-		todos.save(entity);
+		repository.save(entity);
 	}
 
 	@GetMapping("/api/todo")
 	public Iterable<TodoEntity> getTodos() {
-		return todos.findAll();
+		return repository.findAll();
 	}
 }
